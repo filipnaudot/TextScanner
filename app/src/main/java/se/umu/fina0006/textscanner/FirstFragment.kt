8f8fd8bfccd27b3b7151f8bf0d82ddc65be65d91
filip <filip.naudot@gmail.com>
2023-07-27 15:04:37 +0200

    cleaning

--- app/src/main/java/se/umu/fina0006/textscanner/FirstFragment.kt
+++ app/src/main/java/se/umu/fina0006/textscanner/FirstFragment.kt
@@ -5,18 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import se.umu.fina0006.textscanner.databinding.FragmentFirstBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
+
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -31,12 +27,6 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        /*
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
-         */
     }
 
     override fun onDestroyView() {
