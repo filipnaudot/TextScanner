62f216ca49110b25719bb44fd9b3f870429d0768
filip <filip.naudot@gmail.com>
2023-08-10 07:56:32 +0200

    checks settings before auto copy

--- app/src/main/java/se/umu/fina0006/textscanner/SecondFragment.kt
+++ app/src/main/java/se/umu/fina0006/textscanner/SecondFragment.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceManager
 import se.umu.fina0006.textscanner.databinding.FragmentSecondBinding
 
 
@@ -36,8 +37,11 @@ class SecondFragment : Fragment() {
 
 
         // Copy button
-        // if setting.autoCopy
-        copyToPrimaryClip(scannedText)
+        val sharedPreferences = activity?.let { PreferenceManager.getDefaultSharedPreferences(it) }
+        val autoCopy = sharedPreferences?.getBoolean("CopyToClipboard", false)
+        if (autoCopy == true) {
+            copyToPrimaryClip(scannedText)
+        }
     }
 
     override fun onDestroyView() {
