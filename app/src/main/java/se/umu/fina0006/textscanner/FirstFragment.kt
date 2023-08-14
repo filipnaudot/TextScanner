503bb4cc1e51b1a03de9c499fb4b5690d335e747
filip <filip.naudot@gmail.com>
2023-08-14 12:28:43 +0200

    stores scan results to app local storage as json

--- app/src/main/java/se/umu/fina0006/textscanner/FirstFragment.kt
+++ app/src/main/java/se/umu/fina0006/textscanner/FirstFragment.kt
@@ -1,5 +1,6 @@
 package se.umu.fina0006.textscanner
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,14 +8,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.fragment.app.activityViewModels
+import org.json.JSONArray
 import se.umu.fina0006.textscanner.databinding.FragmentFirstBinding
+import java.io.IOException
 
 
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
     private val sharedViewModel: SharedViewModel by activityViewModels()
-    private val scanResultList = mutableListOf<String>()
+    private var scanResultList = mutableListOf<String>()
     private lateinit var adapter: ArrayAdapter<String>
 
     // This property is only valid between onCreateView and onDestroyView.
@@ -33,12 +36,44 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        readScanResultFromJson()
         adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, scanResultList)
         binding.scanResultList.adapter = adapter
 
         sharedViewModel.scanStorage.observe(viewLifecycleOwner) { newText ->
             scanResultList.add(newText)
             adapter.notifyDataSetChanged()
+            storeScanResultToJson()
+        }
+    }
+
+    private fun readScanResultFromJson() {
+        scanResultList = mutableListOf()
+
+        try {
+            val fileInputStream = context?.openFileInput("scan_results")
+            val byteArray = fileInputStream?.readBytes()
+            val jsonArray = JSONArray(byteArray?.toString(Charsets.UTF_8))
+
+            for (i in 0 until jsonArray.length()) {
+                scanResultList.add(jsonArray.getString(i))
+            }
+
+            fileInputStream?.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun storeScanResultToJson() {
+        val jsonArray = JSONArray(scanResultList)
+
+        try {
+            val fileOutputStream = context?.openFileOutput("scan_results", Context.MODE_PRIVATE)
+            fileOutputStream?.write(jsonArray.toString().toByteArray())
+            fileOutputStream?.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
     }
 
