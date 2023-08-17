ef6d6c12ec8ed1d26764248cedf99c4be946ecb7
filip <filip.naudot@gmail.com>
2023-08-17 12:50:38 +0200

    function comment and added Log.d to copyToPrimaryClip

--- app/src/main/java/se/umu/fina0006/textscanner/SecondFragment.kt
+++ app/src/main/java/se/umu/fina0006/textscanner/SecondFragment.kt
@@ -5,6 +5,7 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -32,6 +33,14 @@ class SecondFragment : Fragment() {
 
     }
 
+    /**
+     * Called when the associated view is created.
+     * Initializes UI elements and handles user interactions
+     * for displaying scan details and performing actions related to the scanned text.
+     *
+     * @param view The root view of the fragment.
+     * @param savedInstanceState The saved state of the fragment, if available.
+     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val scannedText = arguments?.getString("scannedText")
@@ -50,16 +59,23 @@ class SecondFragment : Fragment() {
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
+    /**
+     * Checks if auto copy setting is on.
+     * If should auto copy, copies the provided text to the device's primary clipboard.
+     *
+     * @param text The text to be copied to the clipboard.
+     */
     private fun copyToPrimaryClip(text : String?) {
+        Log.d(TAG, "copyToPrimaryClip")
         val clipboard = requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
         val clip: ClipData = ClipData.newPlainText("Text produced by MLKit text recognition", text)
         clipboard.setPrimaryClip(clip)
         Toast.makeText(requireContext(), "Text copied to clipboard", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
