052040c9ccb46d338f90748995dd7bf115f9e173
filip <filip.naudot@gmail.com>
2023-08-19 14:10:25 +0200

    comment for SharedViewModel class

--- app/src/main/java/se/umu/fina0006/textscanner/SharedViewModel.kt
+++ app/src/main/java/se/umu/fina0006/textscanner/SharedViewModel.kt
@@ -3,6 +3,9 @@ package se.umu.fina0006.textscanner
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+/**
+ * ViewModel class that holds shared data between ListFragment and EditFragment.
+ */
 class SharedViewModel : ViewModel() {
     var lastEmittedValue: String? = null
     val scanStorage = MutableLiveData<String>()
